5fb893b264e4e908ce72ee6de79069c1c6d689f6
leirisson <103783610+leirisson@users.noreply.github.com>
2023-10-08 18:06:16 -0400

    vai jailson

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,7 +2,7 @@ import './Banner.css'
 function Banner(){  
     return(
     <header className="banner">   
-        <div class="swiper-slide"><img src=" https://i.ibb.co/d0gNX5Q/img1.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
+        <div class="swiper-slide"><img src="https://i.ibb.co/4j47HFf/img5.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
         <div class="swiper-slide"><img src="https://i.ibb.co/ncfs6Kp/img2.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
         <div class="swiper-slide"><img src="https://i.ibb.co/CzqBkWX/img3.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
         <div class="swiper-slide"><img src="https://i.ibb.co/jRs4bYH/img4.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
