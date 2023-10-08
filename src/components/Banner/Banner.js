7919a226aa1c3e39d491fe8d26ad18f92bac65a8
leirisson <103783610+leirisson@users.noreply.github.com>
2023-10-08 18:02:59 -0400

    subir

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,12 +2,19 @@ import './Banner.css'
 function Banner(){  
     return(
     <header className="banner">   
-        <div class="swiper-slide"><img src="imagens/_banner/img2.jpeg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
-        <div class="swiper-slide"><img src="imagens/_banner/img3.jpeg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
-        <div class="swiper-slide"><img src="imagens/_banner/img4.jpeg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
-        <div class="swiper-slide"><img src="imagens/_banner/img5.jpeg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
-        <div class="swiper-slide"><img src="imagens/_banner/img6.jpeg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
+        <div class="swiper-slide"><img src=" https://i.ibb.co/d0gNX5Q/img1.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
+        <div class="swiper-slide"><img src="https://i.ibb.co/ncfs6Kp/img2.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
+        <div class="swiper-slide"><img src="https://i.ibb.co/CzqBkWX/img3.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
+        <div class="swiper-slide"><img src="https://i.ibb.co/jRs4bYH/img4.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
+        <div class="swiper-slide"><img src="https://i.ibb.co/2vfjLyh/img6.jpg" className="img-banner" alt="imagem do barbeiro cortando Jailson"/></div>
     </header>
     )}
 
+
+    // https://i.ibb.co/d0gNX5Q/img1.jpg
+    // https://i.ibb.co/ncfs6Kp/img2.jpg
+    // https://i.ibb.co/CzqBkWX/img3.jpg
+    // https://i.ibb.co/jRs4bYH/img4.jpg
+    // https://i.ibb.co/4j47HFf/img5.jpg
+    // https://i.ibb.co/2vfjLyh/img6.jpg
 export default Banner
